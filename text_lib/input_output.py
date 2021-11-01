857b3bfcc0835cd5a09698b8b22ab4c58f605207
Kartikey Tewari <kartikeya30@gmail.com>
2021-11-02 01:00:10 +0530

    add text_lib/input_output.py module

---
+++ text_lib/input_output.py
@@ -0,0 +1,5 @@
+def print_summary(raw_text, summary_text):
+    print(summary_text)
+
+    print("length of original text = " + str(len(raw_text)))
+    print("length of summary text = " + str(len(summary_text)))
