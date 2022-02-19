81debab5be5330efbd5e974e988ae64e08e786ad
Kartikey Tewari <kartikeya30@gmail.com>
2022-02-20 02:28:08 +0530

    data visualisation for the:
    - 10 documents [0-9]
    - 10 gradients [1-10]

--- data_import.py
+++ data_import.py
@@ -19,7 +19,7 @@ os.system("mkdir ./input/multi_news_input/summary")
 old_output=sys.stdout
 count=0
 for i in ds:
-    if (count<=10):
+    if (count<=9):
         file_name_document="file://./input/multi_news_input/document/" + str(count)
         # sys.stdout=file_name_document
         tf.print(i["document"], output_stream=file_name_document)
