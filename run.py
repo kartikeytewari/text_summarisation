f109fe896a16e55aa3a113c179b8aa66b9df4c27
Kartikey Tewari <kartikeya30@gmail.com>
2022-02-28 01:08:33 +0530

    Remove local_file_weight from run.py

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ for i in file_list:
 global_token_score=gen_global_token_score(file_freq)
 rouge=Rouge()
 # print first line of excel sheet
-print("local_file_weight, local_file, rouge-1 f1-score, rouge-1 precision, rouge-1 recall,rouge-2 f1-score, rouge-2 precision, rouge-2 recall, rouge-l f1-score, rouge-l precision, rouge-l recall, bleu-1, bleu-2, bleu-3, bleu-4")
+print("local_file, rouge-1 f1-score, rouge-1 precision, rouge-1 recall,rouge-2 f1-score, rouge-2 precision, rouge-2 recall, rouge-l f1-score, rouge-l precision, rouge-l recall, bleu-1, bleu-2, bleu-3, bleu-4")
 local_file_weight=95.0
 for local_file in file_list:
 
@@ -51,8 +51,7 @@ for local_file in file_list:
     ref_summary_token.append(token_gen(ref_summary))
 
     # print metrics
-    print(
-        str(local_file_weight) + "," + 
+    print( 
         str(local_file) + "," + 
         str(rouge_score["rouge-1"]["f"]) + "," + 
         str(rouge_score["rouge-1"]["p"]) + "," + 
