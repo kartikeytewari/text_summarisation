007b64c8c5ebec95bdd85e4767b9cd10fc4914ba
Kartikey Tewari <kartikeya30@gmail.com>
2022-05-03 05:09:58 +0530

    rebuild multi_news_input on all dataset

--- data_import.py
+++ data_import.py
@@ -20,15 +20,14 @@ old_output=sys.stdout
 count=0
 print ("count = " + str(count))
 for i in ds:
-    if (count<=10):
-        print ("count = " + str(count))
-        file_name_document="file://./input/multi_news_input/document/" + str(count)
-        # sys.stdout=file_name_document
-        tf.print(i["document"], output_stream=file_name_document)
-        sys.stdout.flush()
+    print ("count = " + str(count))
+    file_name_document="file://./input/multi_news_input/document/" + str(count)
+    # sys.stdout=file_name_document
+    tf.print(i["document"], output_stream=file_name_document)
+    sys.stdout.flush()
 
-        file_name_summary="file://./input/multi_news_input/summary/" + str(count)
-        # sys.stdout=file_name_summary
-        tf.print(i["summary"], output_stream=file_name_summary)
-        sys.stdout.flush()
-        count+=1
+    file_name_summary="file://./input/multi_news_input/summary/" + str(count)
+    # sys.stdout=file_name_summary
+    tf.print(i["summary"], output_stream=file_name_summary)
+    sys.stdout.flush()
+    count+=1
